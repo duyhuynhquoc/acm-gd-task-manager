30f59d12f4f3f01a8d2ce6723a767c46b161b0d0
Duy Huynh Quoc <duyhq.zee@gmail.com>
2021-12-17 00:19:14 +0700

    Add .env file for server

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 indexRouter(app);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on PORT ${PORT}`);
